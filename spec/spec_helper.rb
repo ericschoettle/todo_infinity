acdd55502c53c73cacb2b3a63a527bf57d658ab0
Eric Schoettle and zach beecher <eric@>
2017-05-08 11:46:23 -0700

    template todo with active directory

---
+++ spec/spec_helper.rb
@@ -0,0 +1,15 @@
+ENV['RACK_ENV'] = 'test'
+
+require('rspec')
+require('pg')
+require('sinatra/activerecord')
+require('task')
+require('list')
+
+RSpec.configure do |config|
+  config.after(:each) do
+    Task.all().each do |task|
+      task.destroy()
+    end
+  end
+end
